73c74f3e14682dd0d36e45582eadffb0e201dc10
Luis Romero <luisangel930115@gmail.com>
2025-11-30 15:27:57 -0500

    Configurar SendGrid MailService y variables env

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,6 +1,6 @@
 // src/mail/mail.service.ts
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
-import * as sgMail from '@sendgrid/mail';
+import sgMail from '@sendgrid/mail';
 
 @Injectable()
 export class MailService {
